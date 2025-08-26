93d19d480d945cf45732bb67bab6ff7d10417f43
Md Zeyad Ali <mdzeyadali29@gmail.com>
2025-08-26 23:47:08 +0530

    Add

--- controller/Transaction.js
+++ controller/Transaction.js
@@ -1,27 +1,17 @@
 const express = require('express');
-const router = express.Router();
 const Transaction = require('../model/Transaction');
 
-
-//post handler 
-// router.post('/addTransaction',
     
 exports.createTransaction =    async(req,res)=>{
-    try{
- const transaction = new Transaction(req.body)
-     await transaction.save();
-     res.status(200).json(transaction);
-    }catch(error){
-        res.status(400).json({message:error.message});
-    }
+   try{
+ const transaction = new Transaction (req.body);
+ await transaction.save();
+ res.status(201).json(transaction);
+   }catch(error){
+    console.log(error);
+   }
 }
    
-// )
-
-// get handler 
-
-// router.get('/getTransactions',
-
 exports.getTransactions =    async(req,res)=>{
     try{
         const transactions = await Transaction.find();
@@ -30,7 +20,6 @@ exports.getTransactions =    async(req,res)=>{
         res.status(500).json({message:error.message});
     }
 }
-// )
 
 
 
